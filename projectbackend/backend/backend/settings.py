cc4c938c149e2785cf758271980faaf59701c2e0
Gohankr45444 <gohankr45444@gmail.com>
2025-09-06 04:49:01 +0000

    commit 51

--- projectbackend/backend/backend/settings.py
+++ projectbackend/backend/backend/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "whitenoise.storage.CompressedManifestStaticFilesStorage",
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -132,6 +131,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
